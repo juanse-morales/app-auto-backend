0138aaccd99729183ccada27651f4db86d5f9619
Juan Morales <jmoralespuello@gmail.com>
2024-09-11 07:04:33 -0500

    ADD: Set api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AutoController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/autos', [AutoController::class, 'index']);
+
+Route::get('/autos/{id}', [AutoController::class, 'show']);
+
+Route::post('/autos', [AutoController::class, 'store']);
+
+Route::put('/autos/{id}', [AutoController::class, 'update']);
+
+Route::patch('/autos/{id}', [AutoController::class, 'updatePartial']);
+
+Route::delete('/autos/{id}', [AutoController::class, 'destroy']);
